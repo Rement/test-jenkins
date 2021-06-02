20648c6c3d43c13ed42ef779b255b2b166aeb9dc
Andrei Kaliachka <andreikaliachka@Andreis-MacBook-Pro.local>
2021-06-02 19:08:24 +0300

    any fix

--- src/main/java/com/epam/Main.java
+++ src/main/java/com/epam/Main.java
@@ -1,8 +1,11 @@
 package com.epam;
 
+import com.epam.entity.A;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        A a = new A();
     }
 }
---
+++ src/main/java/com/epam/entity/A.java
@@ -0,0 +1,7 @@
+package com.epam.entity;
+
+public class A {
+
+    public int k;
+
+}
